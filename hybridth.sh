266acb1ed424ea3efe1e9dd4bfe962436a679131
hybridth1 <47426946+hybridth1@users.noreply.github.com>
2019-02-08 03:37:07 +0700

    Add files via upload

---
+++ hybridth.sh
@@ -0,0 +1,155 @@
+#!/system/xbin/bash
+clear    
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+echo $red
+echo ""
+echo $blue "Created by :hybridth"
+echo $blue "Team :SPP"
+echo $blue "contact my:bridthh@gmail.com" 
+echo $blue "Enjoy"
+echo
+echo "------------------------------->"
+echo $white "1.Defacer"
+sleep 1					
+echo "------------------------------->"
+echo $white "2.Darkfly-Tools"
+sleep 1
+echo "------------------------------->"
+echo $white "3.Admin-Panel-Finder"
+sleep 1					
+echo "------------------------------->"
+echo $white "4.List Dork"
+sleep 1					
+echo "------------------------------->"
+echo $white "5.Tools-x"
+sleep 1					
+echo "------------------------------->"
+echo $white "6.XAttacker"
+sleep 1					
+echo "------------------------------->"
+echo $white "7.Phising"
+sleep 1 				
+echo "------------------------------->"
+echo $white "8.LTEDDOS"
+sleep 1 				
+echo "------------------------------->"
+echo $white "9.Malicious"
+sleep 1 				
+echo "------------------------------->"
+echo $white "10.IP_Tracker"
+sleep 1 				
+echo "------------------------------->"
+echo $white "0.Out"
+echo "------------------------------->"
+echo "\033[32;1m"
+read -p "Mr.Hybridth-->" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "Mr.Hybridth" | lolcat
+git clone https://github.com/21D4N404/Defacer
+mv Defaver $HOME
+cd $HOME/Defacer
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+figlet "Mr.Hybridth" | lolcat
+git clone https://github.com/Ranginang67/DarkFly-Tool
+mv DarkFly-Tool $HOME
+cd $HOME/DarkFly-Tool
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+figlet "Mr.Hybridth" | lolcat
+git clone https://github.com/the-robot/admin-finder
+mv admin-finder $HOME
+cd $HOME/admin-finder
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+figlet "Mr.Hybridth" | lolcat
+git clone https://github.com/hybridth1/Dork_List/
+mv Dork_List $HOME
+cd $HOME/Dork_List
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/Rajkumrdusad/Tool-X
+mv Tool-X $HOME
+cd $HOME/Tool-X
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/Moham3dRiahi/XAttacker
+mv XAttacker $HOME
+cd $HOME/XAttacker
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/Senitopeng/Mancing
+mv Mancing $HOME
+cd $HOME/Mancing
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/4L13199/LITEDDOS
+mv LITEDDOS $HOME
+cd $HOME/LITEDDOS
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/Hider5/Malicious
+mv Malicious $HOME
+cd $HOME/Malicious
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/Rajkumrdusad/IP-Tracer
+mv IP-Tracer $HOME
+cd $HOME/IP-Tracer
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+echo "Jangan Menyerah Sebelum Berhasil"
+sleep 2
+echo "Karna Semua Perjuangan Tidak Ada Yang Sia-Sia"
+sleep 2
+echo "Teruslah Berusaha Sampai Kau Mempunyai Hasil"
+sleep 2
+echo "Good Luck In Your Day"
+sleep 2
+exit
+fi
