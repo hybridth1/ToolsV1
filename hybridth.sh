6ddad4754f47773b586f127c4188d3ce9ca08126
hybridth1 <47426946+hybridth1@users.noreply.github.com>
2019-02-08 01:34:26 +0700

    Add files via upload

---
+++ hybridth.sh
@@ -0,0 +1,98 @@
+#!/system/xbin/bash
+clear    
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+echo $red
+echo ""
+echo $blue "Created by :hybridth"
+echo $blue "Team :SPP"
+echo $blue "contact my:bridthh@gmail.com" 
+echo $blue "Enjoy"
+echo
+echo "------------------------------->"
+echo $white "1.Defacer"
+echo "------------------------------->"
+echo $white "2.Darkfly-Tools"
+echo "------------------------------->"
+echo $white "3.Admin-Panel-Finder"
+echo "------------------------------->"
+echo $white "4.List Dork"
+echo "------------------------------->"
+echo $white "5.Tools-x"
+echo "------------------------------->"
+echo $white "6.XAttacker"
+echo "------------------------------->"
+echo $white "0.Out"
+echo "------------------------------->"
+echo "\033[32;1m"
+read -p "Mr.Hybridth-->" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "Mr.Hybridth" | lolcat
+git clone https://github.com/21D4N404/Defacer
+mv Defaver $HOME
+cd $HOME/Defacer
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+figlet "Mr.Hybridth" | lolcat
+git clone https://github.com/Ranginang67/DarkFly-Tool
+mv DarkFly-Tool $HOME
+cd $HOME/DarkFly-Tool
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+figlet "Mr.Hybridth" | lolcat
+git clone https://github.com/the-robot/admin-finder
+mv admin-finder $HOME
+cd $HOME/admin-finder
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+figlet "Mr.Hybridth" | lolcat
+git clone https://github.com/hybridth1/Dork_List/
+mv Dork_List $HOME
+cd $HOME/Dork_List
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+figlet "Mr.Hybridth"
+git clone https://github.com/Rajkumrdusad/Tool-X
+mv Tool-X $HOME
+cd $HOME/Tool-X
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+git clone https://github.com/Moham3dRiahi/XAttacker
+mv XAttacker $HOME
+cd $HOME/XAttacker
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+echo "Jangan Menyerah Sebelum Berhasil"
+sleep 2
+echo "Karna Semua Perjuangan Tidak Ada Yang Sia-Sia"
+sleep 2
+echo "Good Luck In Your Day"
+sleep 2
+exit
+fi
